184ace5066d44046aca3ef0eb5eb67ade9daf57d
Zhang Zhengshuo <75479485+2312230978@users.noreply.github.com>
2023-10-21 11:08:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oVzdI6iGp0dwDnC-ll6XGWrEw194',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'PqziIssrwG8o6g2ElMBLsZc2CMnAAOhDZKB0FOXt82Q',
+      useTemplateId: 'rXTmCvIgOIW_aWZA5SCxyhJ9e2evcO6hNgRDWL4CO4E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-19',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'DqIJwz_jzrrnmXku3Xq0bGXl_nbN3-Ia1TOwE_kjNiQ',
+  CALLBACK_TEMPLATE_ID: 'SlbxzmBpV6pKyOLyESuxodvgOB-Gt-gB7sFE2T-owKM',
 
   CALLBACK_USERS: [
     {
